e344a2972e932b53592c4dd28acfb30a1afe7305
Iago Ramone <iagoramone.fisica@gmail.com>
2024-10-04 09:58:46 -0300

    última atualização

--- src/app/converter/converter.module.ts
+++ src/app/converter/converter.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
 
 import { ConverterPageRoutingModule } from './converter-routing.module';
-
 import { ConverterPage } from './converter.page';
 
 @NgModule({
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ExchangeService } from '../services/exchange.service';
 
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
